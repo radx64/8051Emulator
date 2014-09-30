dd28aeacb50c024a418dceb95480184547f3560e
radx64 <radek-team@wp.pl>
2014-09-30 17:18:16 +0200

    More Cmake improvements

---
+++ clean.sh
@@ -0,0 +1,5 @@
+rm CMakeFiles -rf
+rm Emulator8051
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello world!" << std::endl;
+	std::cout << "argc:\t"<< argc << std::endl;
+	std::cout << "argv*:\t"<< argv << std::endl;		
+	return 0;
+}
