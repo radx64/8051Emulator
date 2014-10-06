183517f18f87ff193e78676ff40ba8de97133438
radx64 <radek-team@wp.pl>
2014-10-06 17:21:57 +0200

    Removed unnecessary .o files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radek/Desktop/8051Emulator
-# Build directory: /home/radek/Desktop/8051Emulator/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(test)
--- build/gmock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radek/Desktop/8051Emulator/test/gmock/gtest
-# Build directory: /home/radek/Desktop/8051Emulator/build/gmock/gtest
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
